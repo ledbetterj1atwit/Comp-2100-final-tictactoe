6f80ca7dba28aa3595718022ac9ac81db025a9cc
jll123567 <34464341+jll123567@users.noreply.github.com>
2020-11-18 15:09:08 -0500

    Multiple changes.
    Added checkTie.
    Added checkWin.
    Updated init.
    Updated display.
    Updated .settings to make server and client use utf-8 so the box-drawing
    characters work.
    Added docs.

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,3 @@
-
-/**
- * 
- */
-
 /**
  * 
  *
@@ -12,7 +7,7 @@
  */
 import java.net.*;
 import java.io.*;
-public class Client//test
+public class Client
     {
 
     /**
@@ -43,37 +38,83 @@ public class Client//test
         String serverResponse = "";
         }
     
+    /**
+     * Output the game board.
+     * @param board The board.
+     */
     public static void display(char[][] board) {
-    	System.out.printf("%c,%c,%c%n%c,%c,%c%n%c,%c,%c%n", 
+    	System.out.printf("┏━━━┳━━━┳━━━┓%n┃ %c ┃ %c ┃ %c ┃%n┣━━━╋━━━╋━━━┫%n┃ %c ┃ %c ┃ %c ┃%n┣━━━╋━━━╋━━━┫%n┃ %c ┃ %c ┃ %c ┃%n┗━━━┻━━━┻━━━┛%n", 
     			board[0][0], board[0][1], board[0][2],
     			board[1][0], board[1][1], board[1][2],
     			board[2][0], board[2][1], board[2][2]);
     }
     
+    /**
+     * Reset board xWins and oWins to initial state.
+     * @param board
+     * @param xWins
+     * @param oWins
+     */
     public static void init(char[][] board, boolean[] xWins, boolean oWins[]) {
-    	board = new char[][] {
-    			{'1','2','3'},
-    			{'4','5','6'},
-    			{'7','8','9'}
-    			};
+    	for(int i = 0; i<board.length ;i++) {
+    		for(int j = 0; j<board[i].length; j++) {
+    			board[i][j] = (char) ((j+1+(3*i))+48);
+    		}
+    	}
     	for(int i=0; i<xWins.length;i++) {
     		xWins[i] = true;
     		oWins[i] = true;
     	}
     }
-    public static void updateWins(int enemy, boolean[] wins) {
-    	wins[0] = (enemy%3==1) ? false : true;
-    	wins[1] = (enemy%3==2) ? false : true;
-    	wins[2] = (enemy%3==0) ? false : true;
-    	wins[3] = (enemy<4) ? false : true;
-    	wins[4] = (enemy>3 && enemy < 7) ? false : true;
-    	wins[5] = (enemy>6) ? false : true;
-    	wins[6] = (enemy==1 || enemy==5 || enemy==7) ? false : true;
-    	wins[7] = (enemy==3 || enemy==5 || enemy==9) ? false : true;
+    
+    /**
+     * Update a player's possible wins dependent on opponent move
+     * @param move move made by opponent
+     * @param wins wins possible by player
+     */
+    public static void updateWins(int move, boolean[] wins) {
+    	wins[0] = (move%3==1) ? false : true;
+    	wins[1] = (move%3==2) ? false : true;
+    	wins[2] = (move%3==0) ? false : true;
+    	wins[3] = (move<4) ? false : true;
+    	wins[4] = (move>3 && move < 7) ? false : true;
+    	wins[5] = (move>6) ? false : true;
+    	wins[6] = (move==1 || move==5 || move==7) ? false : true;
+    	wins[7] = (move==3 || move==5 || move==9) ? false : true;
     }
     
-    public static int checkWinner(boolean[] xWins, boolean oWins) {
-    	return 0; // TODO: implemnt me
+    /**
+     * Check your wins and opponent wins to see if there has been a tie.
+     * @param yourWins
+     * @param opponentWins
+     * @return
+     */
+    public static boolean checkTie(boolean[] yourWins, boolean[] opponentWins) {
+    	boolean lost = true;
+    	boolean opponentLost = true;
+    	for(boolean b : yourWins) if(b == true) lost = false;
+    	for(boolean b : opponentWins) if(b == true) opponentLost = false;
+    	
+    	return lost && opponentLost; 
+    }
+    
+    /**
+     * Check if a player has won.
+     * @param board
+     * @param playerWins
+     * @param playerSymb the symbol a player uses('x' or 'o')
+     * @return
+     */
+    public static boolean checkWin(char[][] board, boolean[] playerWins, char playerSymb) {
+    	if(playerWins[0] && board[0][0]==playerSymb && board[1][0]==playerSymb && board[2][0]==playerSymb) return true;
+    	else if(playerWins[1] && board[0][1]==playerSymb && board[1][1]==playerSymb && board[2][1]==playerSymb) return true;
+    	else if(playerWins[2] && board[0][2]==playerSymb && board[1][2]==playerSymb && board[2][2]==playerSymb) return true;
+    	else if(playerWins[3] && board[0][0]==playerSymb && board[0][1]==playerSymb && board[0][2]==playerSymb) return true;
+    	else if(playerWins[4] && board[1][0]==playerSymb && board[1][1]==playerSymb && board[1][2]==playerSymb) return true;
+    	else if(playerWins[5] && board[2][0]==playerSymb && board[2][1]==playerSymb && board[2][2]==playerSymb) return true;
+    	else if(playerWins[6] && board[0][0]==playerSymb && board[1][1]==playerSymb && board[2][2]==playerSymb) return true;
+    	else if(playerWins[7] && board[0][2]==playerSymb && board[1][1]==playerSymb && board[2][0]==playerSymb) return true;
+    	return false;
     }
 
     }
--- src/Server.java
+++ src/Server.java
@@ -1,10 +1,3 @@
-import java.net.ServerSocket ;
-import java.net.Socket ;
-
-/**
- * 
- */
-
 /**
  * 
  *
@@ -13,6 +6,7 @@ import java.net.Socket ;
  *
  */
 import java.io.*;
+import java.net.*;
 public class Server
     {
 
@@ -55,37 +49,83 @@ public class Server
             }
     }
     
+    /**
+     * Output the game board.
+     * @param board The board.
+     */
     public static void display(char[][] board) {
-    	System.out.printf("%c,%c,%c%n%c,%c,%c%n%c,%c,%c%n", 
+    	System.out.printf("┏━━━┳━━━┳━━━┓%n┃ %c ┃ %c ┃ %c ┃%n┣━━━╋━━━╋━━━┫%n┃ %c ┃ %c ┃ %c ┃%n┣━━━╋━━━╋━━━┫%n┃ %c ┃ %c ┃ %c ┃%n┗━━━┻━━━┻━━━┛%n", 
     			board[0][0], board[0][1], board[0][2],
     			board[1][0], board[1][1], board[1][2],
     			board[2][0], board[2][1], board[2][2]);
     }
     
+    /**
+     * Reset board xWins and oWins to initial state.
+     * @param board
+     * @param xWins
+     * @param oWins
+     */
     public static void init(char[][] board, boolean[] xWins, boolean oWins[]) {
-    	board[0][0] = '1';
-    	board[0][1] = '2';
-    	board[0][2] = '3';
-    	board[1][0] = '4';
-    	board[1][1] = '5';
-    	board[1][2] = '6';
-    	board[2][0] = '7';
-    	board[2][1] = '8';
-    	board[2][2] = '9';
+    	for(int i = 0; i<board.length ;i++) {
+    		for(int j = 0; j<board[i].length; j++) {
+    			board[i][j] = (char) ((j+1+(3*i))+48);
+    		}
+    	}
     	for(int i=0; i<xWins.length;i++) {
     		xWins[i] = true;
     		oWins[i] = true;
     	}
     }
-    public static void updateWins(int enemy, boolean[] wins) {
-    	wins[0] = (enemy%3==1) ? false : true;
-    	wins[1] = (enemy%3==2) ? false : true;
-    	wins[2] = (enemy%3==0) ? false : true;
-    	wins[3] = (enemy<4) ? false : true;
-    	wins[4] = (enemy>3 && enemy < 7) ? false : true;
-    	wins[5] = (enemy>6) ? false : true;
-    	wins[6] = (enemy==1 || enemy==5 || enemy==7) ? false : true;
-    	wins[7] = (enemy==3 || enemy==5 || enemy==9) ? false : true;
+    
+    /**
+     * Update a player's possible wins dependent on opponent move
+     * @param move move made by opponent
+     * @param wins wins possible by player
+     */
+    public static void updateWins(int move, boolean[] wins) {
+    	wins[0] = (move%3==1) ? false : true;
+    	wins[1] = (move%3==2) ? false : true;
+    	wins[2] = (move%3==0) ? false : true;
+    	wins[3] = (move<4) ? false : true;
+    	wins[4] = (move>3 && move < 7) ? false : true;
+    	wins[5] = (move>6) ? false : true;
+    	wins[6] = (move==1 || move==5 || move==7) ? false : true;
+    	wins[7] = (move==3 || move==5 || move==9) ? false : true;
+    }
+    
+    /**
+     * Check your wins and opponent wins to see if there has been a tie.
+     * @param yourWins
+     * @param opponentWins
+     * @return
+     */
+    public static boolean checkTie(boolean[] yourWins, boolean[] opponentWins) {
+    	boolean lost = true;
+    	boolean opponentLost = true;
+    	for(boolean b : yourWins) if(b == true) lost = false;
+    	for(boolean b : opponentWins) if(b == true) opponentLost = false;
+    	
+    	return lost && opponentLost; 
+    }
+    
+    /**
+     * Check if a player has won.
+     * @param board
+     * @param playerWins
+     * @param playerSymb the symbol a player uses('x' or 'o')
+     * @return
+     */
+    public static boolean checkWin(char[][] board, boolean[] playerWins, char playerSymb) {
+    	if(playerWins[0] && board[0][0]==playerSymb && board[1][0]==playerSymb && board[2][0]==playerSymb) return true;
+    	else if(playerWins[1] && board[0][1]==playerSymb && board[1][1]==playerSymb && board[2][1]==playerSymb) return true;
+    	else if(playerWins[2] && board[0][2]==playerSymb && board[1][2]==playerSymb && board[2][2]==playerSymb) return true;
+    	else if(playerWins[3] && board[0][0]==playerSymb && board[0][1]==playerSymb && board[0][2]==playerSymb) return true;
+    	else if(playerWins[4] && board[1][0]==playerSymb && board[1][1]==playerSymb && board[1][2]==playerSymb) return true;
+    	else if(playerWins[5] && board[2][0]==playerSymb && board[2][1]==playerSymb && board[2][2]==playerSymb) return true;
+    	else if(playerWins[6] && board[0][0]==playerSymb && board[1][1]==playerSymb && board[2][2]==playerSymb) return true;
+    	else if(playerWins[7] && board[0][2]==playerSymb && board[1][1]==playerSymb && board[2][0]==playerSymb) return true;
+    	return false;
     }
  }
 	// end class Server
