0fd42b6e6addaed0eb8d6989549d478c5900fb55
jll123567 <34464341+jll123567@users.noreply.github.com>
2020-11-18 12:35:33 -0500

    Added Display

--- src/Client.java
+++ src/Client.java
@@ -23,6 +23,13 @@ public class Client
         // TODO Auto-generated method stub
 
         }
+    
+    public static void display(char[][] board) {
+    	System.out.printf("%c,%c,%c%n%c,%c,%c%n%c,%c,%c%n", 
+    			board[0][0], board[0][1], board[0][2],
+    			board[1][0], board[1][1], board[1][2],
+    			board[2][0], board[2][1], board[2][2]);
+    }
 
     }
 	// end class Client
--- src/Server.java
+++ src/Server.java
@@ -18,11 +18,17 @@ public class Server
      *
      * @param args
      */
-    public static void main( String[] args )
-        {
-        // TODO Auto-generated method stub
-
-        }
+    public static void main( String[] args ) {
+        char[][] board = new char[3][3];
+    	// Server is x since they go first
 
     }
+    
+    public static void display(char[][] board) {
+    	System.out.printf("%c,%c,%c%n%c,%c,%c%n%c,%c,%c%n", 
+    			board[0][0], board[0][1], board[0][2],
+    			board[1][0], board[1][1], board[1][2],
+    			board[2][0], board[2][1], board[2][2]);
+    }
+ }
 	// end class Server
