ba5dc6be8f9b3965a7c7bf8325ed1dd53ffd6595
mazurz <mazurz@BY1926A75000213.wit.private>
2020-11-18 12:12:56 -0500

    Second Try

---
+++ src/Client.java
@@ -0,0 +1,28 @@
+
+/**
+ * 
+ */
+
+/**
+ * 
+ *
+ * @author mazurz
+ * @version 1.0.0 2020-11-18 Initial implementation
+ *
+ */
+public class Client
+    {
+
+    /**
+     * 
+     *
+     * @param args
+     */
+    public static void main( String[] args )
+        {
+        // TODO Auto-generated method stub
+
+        }
+
+    }
+	// end class Client
